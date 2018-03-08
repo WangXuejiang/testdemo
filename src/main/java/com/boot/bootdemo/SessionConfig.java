00e8f368c8fe5342b2c4c7d032573ac1f8a4c4a9
WangXuejiang <18983434204@163.com>
2018-03-08 09:57:02 +0800

    这是第一次用IDEA提交项目

--- src/main/java/com/boot/bootdemo/SessionConfig.java
+++ src/main/java/com/boot/bootdemo/SessionConfig.java
@@ -1,13 +1,16 @@
 package com.boot.bootdemo;
 
+import com.boot.bootdemo.dao.StudentDao;
 import com.boot.bootdemo.interceptor.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class SessionConfig extends WebMvcConfigurerAdapter{
-
+    @Autowired
+    private StudentDao studentDao;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
